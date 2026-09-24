5528250ccf0ed7607edaa2e3bae5070e58cbf6d7
KelwinKlinger <soukelwin@gmail.com>
2026-09-24 14:48:47 -0300

    feat: adiciona controller da Pokedex

---
+++ src/controllers/TerminalController.ts
@@ -0,0 +1,48 @@
+import { PokeApiService } from "../services/PokeApiService";
+import { BoxService } from "../services/BoxService";
+import { formatarPokemon } from "../utils/textFormatters";
+
+export class TerminalController {
+  constructor(
+    private readonly pokeApiService: PokeApiService,
+    private readonly boxService: BoxService,
+  ) {}
+
+  async executar(): Promise<void> {
+    console.log("=== Pokédex TypeScript Lite ===");
+
+    await this.buscarEAdicionar("pikachu");
+    await this.exibirCatalogo();
+    await this.removerPokemon(25);
+    await this.exibirCatalogo();
+  }
+
+  private async exibirCatalogo(): Promise<void> {
+    const pokemons = await this.boxService.listar();
+
+    if (pokemons.length === 0) {
+      console.log("Catálogo vazio.");
+      return;
+    }
+
+    console.log("Catálogo atual:");
+
+    pokemons.forEach((pokemon) => {
+      console.log(formatarPokemon(pokemon));
+    });
+  }
+
+  private async buscarEAdicionar(nomeOuId: string | number): Promise<void> {
+    const pokemon = await this.pokeApiService.buscar(nomeOuId);
+
+    await this.boxService.adicionar(pokemon);
+
+    console.log(`${pokemon.name} foi adicionado à coleção.`);
+  }
+
+  private async removerPokemon(id: number): Promise<void> {
+    await this.boxService.remover(id);
+
+    console.log(`Pokémon com ID ${id} foi removido da coleção.`);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
+import { TerminalController } from "./controllers/TerminalController";
 import { PokeApiService } from "./services/PokeApiService";
 import { BoxService } from "./services/BoxService";
-import { formatarPokemon } from "./utils/textFormatters";
 
 async function main(): Promise<void> {
   const pokeApiService = new PokeApiService();
   const boxService = new BoxService();
 
+  const controller = new TerminalController(
+    pokeApiService,
+    boxService
+  );
+
   try {
-    const pokemons = await boxService.listar();
-
-    if (pokemons.length === 0) {
-      console.log("Catálogo vazio.");
-      return;
-    }
-
-    console.log("Catálogo atual:");
-
-    pokemons.forEach((pokemon) => {
-      console.log(formatarPokemon(pokemon));
-    });
+    await controller.executar();
   } catch (erro) {
     if (erro instanceof Error) {
       console.error(erro.message);
